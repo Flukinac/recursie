9c800d98ae7d659ee351bf761707bf43b8236039
Steven Visser <sevisser1@gmail.com>
2018-04-05 14:16:43 +0200

    twee recursies

---
+++ recursie.php
@@ -0,0 +1,18 @@
+<?php
+//looping();
+//function looping(){
+//    
+//        if (empty($i)){
+//            $i=1;
+//            looping();
+//        }else{
+//            $i++;
+//            echo $i."<br>";
+//            looping();
+//        }
+//}
+$a=1;
+while($a >= 1){
+    $a++;
+    echo $a;
+}
